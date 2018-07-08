7d6adeb21f0b28f7a3ef150c82d8a78a64e5e575
Andy Stewart <lazycat.manatee@gmail.com>
2018-07-08 11:54:37 +0800

    Add new option `sdcv-tooltip-border-width'.

--- site-lisp/config/init-sdcv.el
+++ site-lisp/config/init-sdcv.el
@@ -93,21 +93,28 @@
         "懒虫简明汉英词典"
         "KDic11万英汉词典"))
 (setq sdcv-dictionary-complete-list     ;星际译王的词典, 完全, 详细
-      '("KDic11万英汉词典"
+      '(
         "懒虫简明英汉词典"
-        "朗道英汉字典5.0"
-        "XDICT英汉辞典"
-        "朗道汉英字典5.0"
-        "XDICT汉英辞典"
-        "懒虫简明汉英词典"
-        "牛津英汉双解美化版"
-        "stardict1.3英汉辞典"
         "英汉汉英专业词典"
-        "CDICT5英汉辞典"
+        "XDICT英汉辞典"
+        "stardict1.3英汉辞典"
+        "WordNet"
+        "XDICT汉英辞典"
         "Jargon"
+        "懒虫简明汉英词典"
         "FOLDOC"
-        "WordNet"))
+        "新世纪英汉科技大词典"
+        "KDic11万英汉词典"
+        "朗道汉英字典5.0"
+        "CDICT5英汉辞典"
+        "新世纪汉英科技大词典"
+        "牛津英汉双解美化版"
+        "21世纪双语科技词典"
+        "quick_eng-zh_CN"
+        ))
 (setq sdcv-dictionary-data-dir lazycat-emacs-sdcv-data-dir) ;设置星际译王本地词典的目录
+
+
 
 (provide 'init-sdcv)
 
