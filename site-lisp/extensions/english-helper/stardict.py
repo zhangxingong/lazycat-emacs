1cbd3c545d931867c9caa8c5bfdb7fd00de3840e
Andy Stewart <lazycat.manatee@gmail.com>
2018-07-20 15:40:19 +0800

    Adjust stardict.el

--- site-lisp/extensions/english-helper/stardict.py
+++ site-lisp/extensions/english-helper/stardict.py
@@ -10,7 +10,11 @@ if __name__ == "__main__":
         start_dictionary = Dictionary(sys.argv[1].split(".ifo")[0])
 
         f = codecs.open("company-english-helper-data.el","w","utf-8")
-        f.write("(defconst en-words-completions\n'(\n")
+        f.write(";; -*- mode: fundamental; -*-\n")
+        f.write(";;\n")
+        f.write(";; This file is generate by stardict.py, please don't edit this file.\n")
+        f.write(";;\n\n")
+        f.write("(defconst en-words-completions\n  '(\n")
 
         index = 0
         for word in start_dictionary.keys():
@@ -18,11 +22,11 @@ if __name__ == "__main__":
             if is_english_word:
                 first_line_translation = start_dictionary.dict[word].split()[0]
                 no_phonetic_translation = first_line_translation.split(">")[-1]
-                
+
                 candiate_word  = word.lower().replace('\"', ' ')
                 candiate_translateion = no_phonetic_translation.strip().replace('\"', ' ')
-                f.write('#(\"'  + candiate_word + '\" ' + '0 1' + '\n' +
-                     '(:initials \"' + candiate_translateion  + '\"))\n')
+                f.write('    #(\"'  + candiate_word + '\" ' + '0 1' + '\n' +
+                     '      (:initials \"' + candiate_translateion  + '\"))\n')
 
                 print(index, candiate_word, candiate_translateion)
                 index += 1
