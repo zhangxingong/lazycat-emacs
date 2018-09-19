0b34f7e2afe79a84415898e939e8b498f9662065
Andy Stewart <lazycat.manatee@gmail.com>
2018-09-19 14:34:41 +0800

    Add cache-path-from-shell.

--- site-lisp/config/init.el
+++ site-lisp/config/init.el
@@ -6,6 +6,7 @@
 
 (with-temp-message ""                   ;抹掉插件启动的输出
   ;; 必须加载的
+  (require 'cache-path-from-shell)
   (require 'lazycat-theme)
   (require 'init-startup)
   (require 'init-flycheck)
