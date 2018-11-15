d651a86c6ebc00ee77116377e04b79fe99a4df56
Andy Stewart <lazycat.manatee@gmail.com>
2018-11-16 01:08:22 +0800

    Remove awesome-pair from minibuffer buffer, It will affect the query history in the minibuffer

--- site-lisp/config/init-awesome-pair.el
+++ site-lisp/config/init-awesome-pair.el
@@ -102,7 +102,6 @@
                'qmake-mode-hook
                'lua-mode-hook
                'swift-mode-hook
-               'minibuffer-inactive-mode-hook
                ))
   (add-hook hook '(lambda () (awesome-pair-mode 1))))
 
