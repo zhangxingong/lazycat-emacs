fa6ccaae3f02260515129fde7ec125060c0f18c7
Andy Stewart <lazycat.manatee@gmail.com>
2019-02-23 16:27:29 +0800

    Improve the performance of rendering long lines.

--- site-lisp/config/init-performance.el
+++ site-lisp/config/init-performance.el
@@ -94,6 +94,9 @@
 (add-hook 'minibuffer-setup-hook #'max-gc-limit)
 (add-hook 'minibuffer-exit-hook #'reset-gc-limit)
 
+;; Improve the performance of rendering long lines.
+(setq-default bidi-display-reordering nil)
+
 (provide 'init-performance)
 
 ;;; init-performance.el ends here
