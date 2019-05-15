43e1f5a6e25b87972312653575c58b9892d75300
Andy Stewart <lazycat.manatee@gmail.com>
2019-05-15 21:36:48 +0800

    Don't set vc-handle-backends, otherwise project-current can't work.

--- site-lisp/config/init-startup.el
+++ site-lisp/config/init-startup.el
@@ -91,7 +91,6 @@
 (tool-bar-mode -1)                      ;禁用工具栏
 (menu-bar-mode -1)                      ;禁用菜单栏
 (scroll-bar-mode -1)                    ;禁用滚动条
-(setq vc-handled-backends nil)          ;禁止版本控制工具，加速启动
 
 (if (featurep 'cocoa)
     (progn
