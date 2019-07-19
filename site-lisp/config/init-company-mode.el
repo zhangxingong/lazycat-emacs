d338be72c11f4dc0a1cbb99355649ca20ce02598
Andy Stewart <lazycat.manatee@gmail.com>
2019-07-19 11:48:52 +0800

    Enable M-number keys.

--- site-lisp/config/init-company-mode.el
+++ site-lisp/config/init-company-mode.el
@@ -173,11 +173,7 @@
  company-mode-map)                      ;unset default keys
 
 (lazy-load-unset-keys
- '("M-p" "M-n" "M-1"
-   "M-2" "M-3" "M-4"
-   "M-5" "M-6" "M-7"
-   "M-8" "M-9" "M-0"
-   "C-m")
+ '("M-p" "M-n" "C-m")
  company-active-map)
 
 (lazy-load-set-keys
