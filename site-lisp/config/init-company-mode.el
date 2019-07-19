db631a89b0ce10c71768b890eee59015ae515bdd
Andy Stewart <lazycat.manatee@gmail.com>
2019-07-19 11:45:21 +0800

    Disble TabNine purchase message.

--- site-lisp/config/init-company-mode.el
+++ site-lisp/config/init-company-mode.el
@@ -130,6 +130,16 @@
 ;; TabNine
 (add-to-list 'company-backends #'company-tabnine)
 
+;; The free version of TabNine is good enough,
+;; and below code is recommended that TabNine not always
+;; prompt me to purchase a paid version in a large project.
+(defadvice company-echo-show (around disable-tabnine-upgrade-message activate)
+  (let ((company-message-func (ad-get-arg 0)))
+    (when (and company-message-func
+               (stringp (funcall company-message-func)))
+      (unless (string-match "The free version of TabNine only indexes up to" (funcall company-message-func))
+        ad-do-it))))
+
 ;; Trigger completion immediately.
 (setq company-idle-delay 0)
 
