70ea27716bbc61d0a44256ac20823e248f394b3d
Andy Stewart <lazycat.manatee@gmail.com>
2019-08-13 20:57:48 +0800

    Improve pyim performance

--- site-lisp/config/init-pyim.el
+++ site-lisp/config/init-pyim.el
@@ -95,6 +95,7 @@
               '(pyim-probe-isearch-mode))
 (setq pyim-page-length 9)
 (setq pyim-posframe-min-width 0)
+(setq pyim-dcache-backend 'pyim-dregcache)
 
 ;; Rime配置
 (liberime-start "/Library/Input Methods/Squirrel.app/Contents/SharedSupport" (file-truename "~/.emacs.d/pyim/rime/"))
