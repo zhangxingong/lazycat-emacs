b3164068c372fdad60ad7d0a2b31b62e605e33ea
Andy Stewart <lazycat.manatee@gmail.com>
2020-03-20 15:48:15 +0800

    upgrade eaf.

--- site-lisp/config/init-eaf.el
+++ site-lisp/config/init-eaf.el
@@ -111,6 +111,7 @@
 (eaf-setq eaf-browser-aria2-proxy-host "127.0.0.1")
 (eaf-setq eaf-browser-aria2-proxy-port "9888")
 (eaf-setq eaf-browser-dark-mode "true")
+(eaf-setq eaf-mindmap-dark-mode "true")
 
 (eaf-setq eaf-marker-letters "JKHLNMUIOYPFDSAVCRREW")
 
