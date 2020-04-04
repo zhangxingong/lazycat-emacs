a19e4325e0e9e48a16c3e10d7010649878e12623
Andy Stewart <lazycat.manatee@gmail.com>
2020-04-05 01:06:06 +0800

    Add org-table-auto-align.el

--- site-lisp/config/init-org.el
+++ site-lisp/config/init-org.el
@@ -84,22 +84,30 @@
 
 ;;; Code:
 
-(setq org-odt-preferred-output-format "docx") ;ODT转换格式默认为docx
-(setq org-startup-folded nil)                 ;默认展开内容
-(setq org-startup-indented t)                 ;默认缩进内容
+(with-eval-after-load 'org
+  (setq org-odt-preferred-output-format "docx") ;ODT转换格式默认为docx
+  (setq org-startup-folded nil)                 ;默认展开内容
+  (setq org-startup-indented t)                 ;默认缩进内容
 
-(defun org-export-docx ()
-  (interactive)
-  (let ((docx-file (concat (file-name-sans-extension (buffer-file-name)) ".docx"))
-        (template-file (concat (file-name-as-directory lazycat-emacs-root-dir)
-                               (file-name-as-directory "template")
-                               "template.docx")))
-    (shell-command (format "pandoc %s -o %s --reference-doc=%s"
-                           (buffer-file-name)
-                           docx-file
-                           template-file
-                           ))
-    (message "Convert finish: %s" docx-file)))
+  (defun org-export-docx ()
+    (interactive)
+    (let ((docx-file (concat (file-name-sans-extension (buffer-file-name)) ".docx"))
+          (template-file (concat (file-name-as-directory lazycat-emacs-root-dir)
+                                 (file-name-as-directory "template")
+                                 "template.docx")))
+      (shell-command (format "pandoc %s -o %s --reference-doc=%s"
+                             (buffer-file-name)
+                             docx-file
+                             template-file
+                             ))
+      (message "Convert finish: %s" docx-file))))
+
+(dolist (hook (list
+               'org-mode-hook
+               ))
+  (add-hook hook '(lambda ()
+                    (require 'org-table-auto-align)
+                    (org-table-auto-align-mode))))
 
 (provide 'init-org)
 
