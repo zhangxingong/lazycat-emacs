92b5771638b17cafc2f6e2291930b58341dfe8d8
Andy Stewart <lazycat.manatee@gmail.com>
2020-04-10 17:14:08 +0800

    Upgrade eaf config.

--- site-lisp/config/init-eaf.el
+++ site-lisp/config/init-eaf.el
@@ -110,6 +110,7 @@
 (eaf-setq eaf-browser-default-zoom "1.25")
 (eaf-setq eaf-browser-aria2-proxy-host "127.0.0.1")
 (eaf-setq eaf-browser-aria2-proxy-port "9888")
+(eaf-setq eaf-terminal-font-size "18")
 
 (eaf-setq eaf-marker-letters "JKHLNMUIOYPFDSAVCRREW")
 
