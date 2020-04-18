a33b3ee30927c7fce01bbf587290c17bb4a83f43
Andy Stewart <lazycat.manatee@gmail.com>
2020-04-18 09:05:52 +0800

    Update nox font.

--- site-lisp/config/init-font.el
+++ site-lisp/config/init-font.el
@@ -93,7 +93,10 @@
     (setq emacs-font-name "WenQuanYi Micro Hei Mono")))
   (when (display-grayscale-p)
     (set-frame-font (format "%s-%s" (eval emacs-font-name) (eval emacs-font-size)))
-    (set-fontset-font (frame-parameter nil 'font) 'unicode (eval emacs-font-name))))
+    (set-fontset-font (frame-parameter nil 'font) 'unicode (eval emacs-font-name))
+
+    (setq nox-doc-tooltip-font (format "%s-%s" emacs-font-name emacs-font-size))
+    ))
 
 (with-eval-after-load 'org
   (defun org-buffer-face-mode-variable ()
