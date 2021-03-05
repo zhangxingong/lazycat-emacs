4ba99b11a1d4012667b4159512f75213ada60e6d
Andy Stewart <lazycat.manatee@gmail.com>
2021-03-05 09:45:54 +0800

    Always use dark theme.

--- site-lisp/config/init.el
+++ site-lisp/config/init.el
@@ -25,7 +25,7 @@
 
     (require 'init-generic)
     (require 'lazycat-theme)
-    (lazycat-theme-load-with-sunrise)
+    (lazycat-theme-load-dark)
     (when (featurep 'cocoa)
       (require 'cache-path-from-shell))
     (require 'lazy-load)
