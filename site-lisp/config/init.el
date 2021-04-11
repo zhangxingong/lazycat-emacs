5ea05262af9dcfb1ddbe7338d597b063ef351af0
Andy Stewart <lazycat.manatee@gmail.com>
2021-04-11 15:07:27 +0800

    Use setenv set proxy.

--- site-lisp/config/init.el
+++ site-lisp/config/init.el
@@ -79,6 +79,7 @@
          (require 'init-nox)
          (require 'init-sdcv)           ; Don't remove this line
          (require 'init-elfeed)
+         (require 'init-proxy)
 
          ;; Restore session at last.
          (require 'init-session)
