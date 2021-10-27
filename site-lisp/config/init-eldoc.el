629d870fdcaad1509be9b0fa52a1aa16e5516b4d
Andy Stewart <lazycat.manatee@gmail.com>
2021-10-27 08:08:06 +0800

    Enable eldoc again.

--- site-lisp/config/init-eldoc.el
+++ site-lisp/config/init-eldoc.el
@@ -76,7 +76,6 @@
 
 ;;; Code:
 
-(global-eldoc-mode -1)
 (dolist (hook (list
                'ielm-mode-hook
                'emacs-lisp-mode-hook
@@ -87,8 +86,12 @@
                'org-mode-hook
                ))
   (add-hook hook #'(lambda ()
-                     (eldoc-mode -1)
-                     (setq eldoc-documentation-function #'ignore) )))
+                     (require 'eldoc)
+                     (require 'eldoc-extension)
+                     (setq eldoc-idle-delay 0) ;显示延迟
+                     (setq eldoc-argument-case 'eldoc-argument-list) ;高亮函数参数
+                     (turn-on-eldoc-mode)
+                     )))
 
 (provide 'init-eldoc)
 
