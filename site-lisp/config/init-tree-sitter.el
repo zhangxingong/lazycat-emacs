3b65ae11ef393b0e82885b7384f4069837674b96
Andy Stewart <lazycat.manatee@gmail.com>
2021-11-25 23:53:09 +0800

    Add Emacs-Lisp for tree-sitter.

--- site-lisp/config/init-tree-sitter.el
+++ site-lisp/config/init-tree-sitter.el
@@ -90,6 +90,14 @@
 (global-tree-sitter-mode)
 (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode)
 
+;; Add Emacs-Lisp for tree-sitter:
+;;
+;; 1. git clone https://github.com/Wilfred/tree-sitter-elisp
+;; 2. gcc ./src/parser.c -fPIC -I./ --shared -o elisp.so
+;; 3. cp ./elisp.so ~/.tree-sitter-langs/bin (~/.tree-sitter-langs/bin is path of your tree-sitter-langs repo)
+(tree-sitter-load 'elisp "elisp")
+(add-to-list 'tree-sitter-major-mode-language-alist '(emacs-lisp-mode . elisp))
+
 (provide 'init-tree-sitter)
 
 ;;; init-tree-sitter.el ends here
