0ff357ba564a06790322d39bd1ef45725b7a6dd5
Andy Stewart <lazycat.manatee@gmail.com>
2022-01-01 18:54:13 +0800

    Use set-face-attribute customze default font when pgtk branch.

--- site-lisp/config/init-font.el
+++ site-lisp/config/init-font.el
@@ -84,19 +84,22 @@
 
 ;;; Code:
 
-(let ((emacs-font-size 14)
-      emacs-font-name)
-  (cond
-   ((featurep 'cocoa)
-    (setq emacs-font-name "Monaco"))
-   ((string-equal system-type "gnu/linux")
-    (setq emacs-font-name "WenQuanYi Micro Hei Mono")))
-  (when (display-grayscale-p)
-    (set-frame-font (format "%s-%s" (eval emacs-font-name) (eval emacs-font-size)))
-    (set-fontset-font (frame-parameter nil 'font) 'unicode (eval emacs-font-name))
+(cond ((eq window-system 'pgtk)
+       (set-face-attribute 'default nil :height 140 :family "WenQuanYi Micro Hei Mono"))
+      (t
+       (let ((emacs-font-size 14)
+             emacs-font-name)
+         (cond
+          ((featurep 'cocoa)
+           (setq emacs-font-name "Monaco"))
+          ((string-equal system-type "gnu/linux")
+           (setq emacs-font-name "WenQuanYi Micro Hei Mono")))
+         (when (display-grayscale-p)
+           (set-frame-font (format "%s-%s" (eval emacs-font-name) (eval emacs-font-size)))
+           (set-fontset-font (frame-parameter nil 'font) 'unicode (eval emacs-font-name))
 
-    (setq nox-doc-tooltip-font (format "%s-%s" emacs-font-name emacs-font-size))
-    ))
+           (setq nox-doc-tooltip-font (format "%s-%s" emacs-font-name emacs-font-size))
+           ))))
 
 (provide 'init-font)
 
