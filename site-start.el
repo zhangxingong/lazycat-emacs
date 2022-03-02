31389cbac08089a6610852f1fc8177b26ff1d150
Andy Stewart <lazycat.manatee@gmail.com>
2022-03-02 10:35:19 +0800

    Improve add-subdirs-to-load-path.

--- site-start.el
+++ site-start.el
@@ -7,21 +7,31 @@
 (defun add-subdirs-to-load-path (search-dir)
   (interactive)
   (let* ((dir (file-name-as-directory search-dir)))
-    (dolist (subdir (cl-remove-if
-                     #'(lambda (subdir)
-                         (or (member subdir '("." ".." "dist" "node_modules" "RCS" "CVS" "rcs" "cvs" "__pycache__" ".git" ".gitattributes" ".gitignore" ".gitmodules"))
-                             (not (file-directory-p (concat dir subdir)))))
-                     (directory-files dir)))
+    (dolist (subdir
+             ;; 过滤出不必要的目录，提升Emacs启动速度
+             (cl-remove-if
+              #'(lambda (subdir)
+                  (or
+                   ;; 不是文件的都移除
+                   (not (file-directory-p (concat dir subdir)))
+                   ;; 目录匹配下面规则的都移除
+                   (member subdir '("." ".." ;Linux当前目录和父目录
+                                    "dist" "node_modules" "__pycache__" ;语言相关的模块目录
+                                    "RCS" "CVS" "rcs" "cvs" ".git" ".github")))) ;版本控制目录
+              (directory-files dir)))
       (let ((subdir-path (concat dir (file-name-as-directory subdir))))
+        ;; 目录下有 .el .so .dll 文件的路径才添加到 `load-path' 中，提升Emacs启动速度
         (when (cl-some #'(lambda (subdir-file)
                            (and (file-regular-p (concat subdir-path subdir-file))
+                                ;; .so .dll 文件指非Elisp语言编写的Emacs动态库
                                 (member (file-name-extension subdir-file) '("el" "so" "dll"))))
                        (directory-files subdir-path))
-          ;; NOTE:
-          ;; We should add path to `load-path' by append, otherwise Emacs can't load expected.
+          
+          ;; 注意：`add-to-list' 函数的第三个参数必须为 t ，表示加到列表末尾
+          ;; 这样Emacs会从父目录到子目录的顺序搜索Elisp插件，顺序反过来会导致Emacs无法正常启动
           (add-to-list 'load-path subdir-path t))
 
-        ;; Search subdirectories recursively.
+        ;; 继续递归搜索子目录
         (add-subdirs-to-load-path subdir-path)))))
 
 (add-subdirs-to-load-path "/usr/share/emacs/lazycat")
