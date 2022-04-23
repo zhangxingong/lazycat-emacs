d6e5c84dc5810320900acd3301170a9f0d540bce
Andy Stewart <lazycat.manatee@gmail.com>
2022-04-23 14:13:52 +0800

    Adjust rime font.

--- site-lisp/config/init-rime.el
+++ site-lisp/config/init-rime.el
@@ -88,7 +88,7 @@
 (setq rime-posframe-properties
       (list :background-color "#333333"
             :foreground-color "#dcdccc"
-            :font "WenQuanYi Micro Hei Mono-14"
+            :font "TsangerJinKai03-6763-13"
             :internal-border-width 10))
 
 (setq default-input-method "rime"
