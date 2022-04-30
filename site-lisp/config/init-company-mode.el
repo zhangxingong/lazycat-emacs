9d2480e9a67d53091f70fc2f6ed8ed3c046c08c6
Andy Stewart <lazycat.manatee@gmail.com>
2022-04-30 21:57:13 +0800

    Disable many lsp-mode features.

--- site-lisp/config/init-company-mode.el
+++ site-lisp/config/init-company-mode.el
@@ -119,6 +119,22 @@
               (require 'lsp-pyright)
               
               (setq lsp-log-io t)
+              (setq lsp-headerline-breadcrumb-enable nil)
+              (setq lsp-enable-symbol-highlighting nil)
+              (setq lsp-ui-doc-enable nil)
+              (setq lsp-ui-doc-show-with-cursor nil)
+              (setq lsp-ui-doc-show-with-mouse nil)
+              (setq lsp-lens-enable nil)
+              (setq lsp-ui-sideline-enable nil)
+              (setq lsp-ui-sideline-show-code-actions nil)
+              (setq lsp-ui-sideline-enable nil)
+              (setq lsp-ui-sideline-show-hover nil)
+              (setq lsp-diagnostics-provider :none)
+              (setq lsp-ui-sideline-enable nil)
+              (setq lsp-eldoc-enable-hover nil)
+              (setq lsp-modeline-diagnostics-enable nil)
+              (setq lsp-signature-auto-activate nil)
+              (setq lsp-signature-render-documentation nil)
 
               (add-hook 'python-mode-hook #'lsp-deferred)
 
