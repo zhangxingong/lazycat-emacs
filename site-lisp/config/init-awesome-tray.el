a184101a35d704fccf9a9c0cea3972003b078615
Andy Stewart <lazycat.manatee@gmail.com>
2022-07-22 17:29:31 +0800

    Add last-command module in awesome-tray.

--- site-lisp/config/init-awesome-tray.el
+++ site-lisp/config/init-awesome-tray.el
@@ -83,6 +83,7 @@
 (require 'awesome-tray)
 
 ;;; Code:
+(setq awesome-tray-active-modules '("location" "belong" "file-path" "mode-name" "last-command" "battery" "date"))
 (awesome-tray-mode 1)
 
 (provide 'init-awesome-tray)
