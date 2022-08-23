aaca0f11940bda9c78c5b7b9343898a749c8dbdc
Andy Stewart <lazycat.manatee@gmail.com>
2022-08-23 19:21:51 +0800

    Revert Change default font to MapleMono.

--- site-lisp/config/init-font.el
+++ site-lisp/config/init-font.el
@@ -86,7 +86,7 @@
 
 (defun load-font-setup()
   (cond ((eq window-system 'pgtk)
-         (set-face-attribute 'default nil :height 140 :family "MapleMono"))
+         (set-face-attribute 'default nil :height 140 :family "WenQuanYi Micro Hei Mono"))
         (t
          (let ((emacs-font-size 14)
                (chinese-font-name  "TsangerJinKai03-6763")
@@ -95,7 +95,7 @@
             ((featurep 'cocoa)
              (setq english-font-name "Monaco"))
             ((string-equal system-type "gnu/linux")
-             (setq english-font-name "MapleMono")))
+             (setq english-font-name "WenQuanYi Micro Hei Mono")))
            (when (display-grayscale-p)
              (set-frame-font (format "%s-%s" (eval english-font-name) (eval emacs-font-size)))
              (set-fontset-font (frame-parameter nil 'font) 'unicode (eval english-font-name))
--- site-lisp/config/init-org.el
+++ site-lisp/config/init-org.el
@@ -120,7 +120,7 @@
                      )))
 
 (let ((emacs-font-size 14)
-      (emacs-font-name "MapleMono"))
+      (emacs-font-name "WenQuanYi Micro Hei Mono"))
   (set-frame-font (format "%s-%s" (eval emacs-font-name) (eval emacs-font-size)))
   (set-fontset-font (frame-parameter nil 'font) 'unicode (eval emacs-font-name)))
 
