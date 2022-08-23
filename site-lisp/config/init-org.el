1e537822cf7807713388d77c65b8f7abc7a5ab15
Andy Stewart <lazycat.manatee@gmail.com>
2022-08-23 17:07:41 +0800

    Change default font to MapleMono.

--- site-lisp/config/init-org.el
+++ site-lisp/config/init-org.el
@@ -120,7 +120,7 @@
                      )))
 
 (let ((emacs-font-size 14)
-      (emacs-font-name "WenQuanYi Micro Hei Mono"))
+      (emacs-font-name "MapleMono"))
   (set-frame-font (format "%s-%s" (eval emacs-font-name) (eval emacs-font-size)))
   (set-fontset-font (frame-parameter nil 'font) 'unicode (eval emacs-font-name)))
 
