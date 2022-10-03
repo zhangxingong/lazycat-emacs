7fd3e44ab1cd07ac8334ab5612e5ba9691163d00
Andy Stewart <lazycat.manatee@gmail.com>
2022-10-04 01:01:03 +0800

    Upgrade lsp-bridge names.

--- site-lisp/config/init-key.el
+++ site-lisp/config/init-key.el
@@ -580,16 +580,16 @@
  '("M-." "M-,"))
 (lazy-load-global-keys
  '(
-   ("C-6" . lsp-bridge-lookup-documentation)
+   ("C-6" . lsp-bridge-popup-documentation)
    ("C-7" . lsp-bridge-jump-back)
    ("C-8" . lsp-bridge-jump)
    ("M-," . lsp-bridge-jump-back)
    ("M-." . lsp-bridge-jump)
    ("C-9" . lsp-bridge-find-references)
    ("C-0" . lsp-bridge-rename)
-   ("M-s-j" . lsp-bridge-jump-to-next-diagnostic) ;显示下一个错误
-   ("M-s-k" . lsp-bridge-jump-to-prev-diagnostic) ;显示上一个错误
-   ("M-s-l" . lsp-bridge-ignore-current-diagnostic) ;忽略当前的错误
+   ("M-s-j" . lsp-bridge-diagnostic-jump-next) ;显示下一个错误
+   ("M-s-k" . lsp-bridge-diagnostic-jump-prev) ;显示上一个错误
+   ("M-s-l" . lsp-bridge-diagnostic-ignore) ;忽略当前的错误
    ("M-s-n" . lsp-bridge-popup-documentation-scroll-up) ;向下滚动文档
    ("M-s-p" . lsp-bridge-popup-documentation-scroll-down) ;向上滚动文档
    )
