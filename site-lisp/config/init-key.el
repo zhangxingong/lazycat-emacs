f504be0acc00ca2c801de8e5c148c495e14b7d57
Andy Stewart <lazycat.manatee@gmail.com>
2022-10-10 20:20:11 +0800

    Upgrade lsp-bridge.

--- site-lisp/config/init-key.el
+++ site-lisp/config/init-key.el
@@ -457,7 +457,7 @@
 ;;; --- 英文助手
 (lazy-load-global-keys
  '(
-   ("M-r" . lsp-bridge-toggle-english-helper)  ;英文助手
+   ("M-r" . lsp-bridge-toggle-english-helper) ;英文助手
    )
  "init-lsp-bridge")
 ;;; ### Ido ###
@@ -589,7 +589,8 @@
    ("C-0" . lsp-bridge-rename)
    ("M-s-j" . lsp-bridge-diagnostic-jump-next) ;显示下一个错误
    ("M-s-k" . lsp-bridge-diagnostic-jump-prev) ;显示上一个错误
-   ("M-s-l" . lsp-bridge-diagnostic-ignore) ;忽略当前的错误
+   ("M-s-l" . lsp-bridge-diagnostic-ignore)    ;忽略当前的错误
+   ("M-s-u" . lsp-bridge-diagnostic-copy)      ;拷贝诊断信息
    ("M-s-n" . lsp-bridge-popup-documentation-scroll-up) ;向下滚动文档
    ("M-s-p" . lsp-bridge-popup-documentation-scroll-down) ;向上滚动文档
    )
