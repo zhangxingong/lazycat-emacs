a1ec38fd794276814b5059c100fd20c1bc8b9832
Andy Stewart <lazycat.manatee@gmail.com>
2023-03-09 11:32:56 +0800

    Make window smooth when start.

--- site-lisp/config/init.el
+++ site-lisp/config/init.el
@@ -11,6 +11,16 @@
       ;; 清空避免加载远程文件的时候分析文件。
       (file-name-handler-alist nil))
 
+  ;; 让窗口启动更平滑
+  (setq frame-inhibit-implied-resize t)
+  (setq-default inhibit-redisplay t
+                inhibit-message t)
+  (add-hook 'window-setup-hook
+            (lambda ()
+              (setq-default inhibit-redisplay nil
+                            inhibit-message nil)
+              (redisplay)))
+
   ;; 定义一些启动目录，方便下次迁移修改
   (defvar lazycat-emacs-root-dir (file-truename "~/lazycat-emacs/site-lisp"))
   (defvar lazycat-emacs-config-dir (concat lazycat-emacs-root-dir "/config"))
