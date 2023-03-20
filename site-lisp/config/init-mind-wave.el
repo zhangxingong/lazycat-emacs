634446375b786da8189b8e8c12ae2906a91b28ad
Andy Stewart <lazycat.manatee@gmail.com>
2023-03-21 01:23:06 +0800

    Build one-key for mind-wave

---
+++ site-lisp/config/init-mind-wave.el
@@ -0,0 +1,102 @@
+;;; init-mind-wave.el --- Config for mind wave   -*- lexical-binding: t; -*-
+
+;; Filename: init-mind-wave.el
+;; Description: Config for mind wave
+;; Author: Andy Stewart <lazycat.manatee@gmail.com>
+;; Maintainer: Andy Stewart <lazycat.manatee@gmail.com>
+;; Copyright (C) 2023, Andy Stewart, all rights reserved.
+;; Created: 2023-03-21 01:17:11
+;; Version: 0.1
+;; Last-Updated: 2023-03-21 01:17:11
+;;           By: Andy Stewart
+;; URL: https://www.github.org/manateelazycat/init-mind-wave
+;; Keywords:
+;; Compatibility: GNU Emacs 30.0.50
+;;
+;; Features that might be required by this library:
+;;
+;;
+;;
+
+;;; This file is NOT part of GNU Emacs
+
+;;; License
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+;; Floor, Boston, MA 02110-1301, USA.
+
+;;; Commentary:
+;;
+;; Config for mind wave
+;;
+
+;;; Installation:
+;;
+;; Put init-mind-wave.el to your load-path.
+;; The load-path is usually ~/elisp/.
+;; It's set in your ~/.emacs like this:
+;; (add-to-list 'load-path (expand-file-name "~/elisp"))
+;;
+;; And the following to your ~/.emacs startup file.
+;;
+;; (require 'init-mind-wave)
+;;
+;; No need more.
+
+;;; Customize:
+;;
+;;
+;;
+;; All of the above can customize by:
+;;      M-x customize-group RET init-mind-wave RET
+;;
+
+;;; Change log:
+;;
+;; 2023/03/21
+;;      * First released.
+;;
+
+;;; Acknowledgements:
+;;
+;;
+;;
+
+;;; TODO
+;;
+;;
+;;
+
+;;; Require
+(require 'mind-wave)
+
+;;; Code:
+(one-key-create-menu
+ "MIND-WAVE"
+ '(
+   (("t" . "Translate to english") . mind-wave-translate-to-english)
+   (("d" . "Proofreading doc") . mind-wave-proofreading-doc)
+   (("r" . "Refactory code") . mind-wave-refactory-code)
+   (("c" . "Comment code") . mind-wave-comment-code)
+   (("e" . "Explain code") . mind-wave-explain-code)
+   (("v" . "Summary video") . mind-wave-summary-video)
+   (("w" . "Summary video") . mind-wave-summary-web)
+   )
+ t)
+
+
+(provide 'init-mind-wave)
+
+;;; init-mind-wave.el ends here
