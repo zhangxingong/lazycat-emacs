e13c3e17e1e76269d4b620dff26922a626a0d492
Andy Stewart <lazycat.manatee@gmail.com>
2023-04-19 13:31:18 +0800

    Update modules.

--- site-lisp/config/init-markmacro.el
+++ site-lisp/config/init-markmacro.el
@@ -88,8 +88,9 @@
  "MARK-MACRO"
  '(
    (("w" . "Mark word") . markmacro-mark-words) ;标记当前符号的单词或者选中区域的单词
-   (("l" . "Mark line") . markmacro-mark-lines) ;标记非空行
-   (("c" . "Mark char") . markmacro-mark-chars) ;标记当前字符
+   (("s" . "Mark symbol") . markmacro-mark-symbols) ;标记当前符号
+   (("l" . "Mark line") . markmacro-mark-lines)     ;标记非空行
+   (("c" . "Mark char") . markmacro-mark-chars)     ;标记当前字符
    (("p" . "Mark parameters") . markmacro-mark-parameters) ;标记参数
    (("i" . "Mark imenu") . markmacro-mark-imenus) ;标记函数或变量
    (("h" . "Secondary region set") . markmacro-secondary-region-set) ;设置二级选中区域
