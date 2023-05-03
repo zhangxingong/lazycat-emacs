fbd43f88d9d9ac6231fc0b4e8ba4057b936dffd6
Andy Stewart <lazycat.manatee@gmail.com>
2023-05-03 16:39:02 +0800

    Remove unnecessary settings from init-markdown-mode.el and update wraplish submodule.

--- site-lisp/config/init-markdown-mode.el
+++ site-lisp/config/init-markdown-mode.el
@@ -95,7 +95,6 @@
  markdown-mode-map)
 
 (setq wraplish-add-space-after-comma t)
-(setq wraplish-add-space-after-period t)
 (setq wraplish-add-space-after-chinese-comma t)
 (setq wraplish-add-space-after-chinese-period t)
 (setq wraplish-add-space-after-quote t)
