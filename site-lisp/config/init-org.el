f5e421f127fb0307bc324e3feddcadbb1ec0ea15
Andy Stewart <lazycat.manatee@gmail.com>
2023-07-17 22:53:01 +0800

    Use valign fix org table alignment problem.

--- site-lisp/config/init-org.el
+++ site-lisp/config/init-org.el
@@ -115,24 +115,26 @@
                         ("M-h" . set-mark-command) ;选中激活
                         ("C-c C-o" . eaf-open-url-at-point)
                         )
-                      org-mode-map
-                      )
+                      org-mode-map)
+
+                     (require 'valign)
+                     (valign-mode)
                      )))
 
-(let ((emacs-font-size 14)
-      (emacs-font-name "WenQuanYi Micro Hei Mono"))
-  (set-frame-font (format "%s-%s" (eval emacs-font-name) (eval emacs-font-size)))
-  (set-fontset-font (frame-parameter nil 'font) 'unicode (eval emacs-font-name)))
+;; (let ((emacs-font-size 14)
+;;       (emacs-font-name "WenQuanYi Micro Hei Mono"))
+;;   (set-frame-font (format "%s-%s" (eval emacs-font-name) (eval emacs-font-size)))
+;;   (set-fontset-font (frame-parameter nil 'font) 'unicode (eval emacs-font-name)))
 
-(with-eval-after-load 'org
-  (defun org-buffer-face-mode-variable ()
-    (interactive)
-    (make-face 'width-font-face)
-    (set-face-attribute 'width-font-face nil :font "等距更纱黑体 SC 22")
-    (setq buffer-face-mode-face 'width-font-face)
-    (buffer-face-mode))
+;; (with-eval-after-load 'org
+;;   (defun org-buffer-face-mode-variable ()
+;;     (interactive)
+;;     (make-face 'width-font-face)
+;;     (set-face-attribute 'width-font-face nil :font "等距更纱黑体 SC 22")
+;;     (setq buffer-face-mode-face 'width-font-face)
+;;     (buffer-face-mode))
 
-  (add-hook 'org-mode-hook 'org-buffer-face-mode-variable))
+;;   (add-hook 'org-mode-hook 'org-buffer-face-mode-variable))
 
 (provide 'init-org)
 
