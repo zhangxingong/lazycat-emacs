dd7290503cb9285c194bcd1724f4a12673976c06
Andy Stewart <lazycat.manatee@gmail.com>
2023-07-18 09:29:54 +0800

    Remove org-mode font settings.

--- site-lisp/config/init-org.el
+++ site-lisp/config/init-org.el
@@ -121,21 +121,6 @@
                      (valign-mode)
                      )))
 
-;; (let ((emacs-font-size 14)
-;;       (emacs-font-name "WenQuanYi Micro Hei Mono"))
-;;   (set-frame-font (format "%s-%s" (eval emacs-font-name) (eval emacs-font-size)))
-;;   (set-fontset-font (frame-parameter nil 'font) 'unicode (eval emacs-font-name)))
-
-;; (with-eval-after-load 'org
-;;   (defun org-buffer-face-mode-variable ()
-;;     (interactive)
-;;     (make-face 'width-font-face)
-;;     (set-face-attribute 'width-font-face nil :font "等距更纱黑体 SC 22")
-;;     (setq buffer-face-mode-face 'width-font-face)
-;;     (buffer-face-mode))
-
-;;   (add-hook 'org-mode-hook 'org-buffer-face-mode-variable))
-
 (provide 'init-org)
 
 ;;; init-org.el ends here
