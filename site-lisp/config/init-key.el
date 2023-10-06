1b2321693a5379066ce914e74e0a8845d3f68e7b
Andy Stewart <lazycat.manatee@gmail.com>
2023-10-06 10:25:34 +0800

    Upgrade lsp-bridge.

--- site-lisp/config/init-key.el
+++ site-lisp/config/init-key.el
@@ -541,8 +541,7 @@
    ("C-0" . lsp-bridge-rename)
    ("M-s-j" . lsp-bridge-diagnostic-jump-next) ;显示下一个错误
    ("M-s-k" . lsp-bridge-diagnostic-jump-prev) ;显示上一个错误
-   ("M-s-l" . lsp-bridge-diagnostic-ignore)    ;忽略当前的错误
-   ("M-s-u" . lsp-bridge-diagnostic-copy)      ;拷贝诊断信息
+   ("M-s-l" . lsp-bridge-diagnostic-copy)      ;拷贝诊断信息
    ("M-s-n" . lsp-bridge-popup-documentation-scroll-up) ;向下滚动文档
    ("M-s-p" . lsp-bridge-popup-documentation-scroll-down) ;向上滚动文档
    )
