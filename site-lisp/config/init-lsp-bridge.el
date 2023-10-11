7fbca0e2c2e06c39c62d70246b899f70d0abec35
Andy Stewart <lazycat.manatee@gmail.com>
2023-10-12 02:53:49 +0800

    Upgrade lsp-bridge.

--- site-lisp/config/init-lsp-bridge.el
+++ site-lisp/config/init-lsp-bridge.el
@@ -87,6 +87,7 @@
 
 (setq lsp-bridge-enable-completion-in-minibuffer t)
 (setq lsp-bridge-signature-show-function 'lsp-bridge-signature-show-with-frame)
+(setq lsp-bridge-enable-with-tramp t)
 (setq acm-enable-quick-access t)
 (setq acm-backend-yas-match-by-trigger-keyword t)
 (setq acm-enable-tabnine nil)
