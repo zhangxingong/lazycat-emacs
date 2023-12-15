b93d840d531f2cd16063c0774534276093df01f3
Andy Stewart <lazycat.manatee@gmail.com>
2023-12-16 02:54:12 +0800

    Upgrade holo-layer.

--- site-lisp/config/init-holo-layer.el
+++ site-lisp/config/init-holo-layer.el
@@ -90,7 +90,7 @@
 (setq holo-layer-hide-mode-line t)
 (setq holo-layer-enable-place-info t)
 (setq holo-layer-sort-tab-ui t)
-(setq holo-layer-enable-indent-info t)
+(setq holo-layer-enable-indent-rainbow t)
 
 (holo-layer-enable)
 
