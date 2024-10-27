c3654ddd54c112e732910b23742dec9b9c2d6276
Andy Stewart <lazycat.manatee@gmail.com>
2024-10-27 20:23:29 +0800

    Update config.

--- site-lisp/config/init-aider.el
+++ site-lisp/config/init-aider.el
@@ -85,7 +85,11 @@
 (require 'aider)
 
 ;;; Code:
-(setq aider-args '("--no-auto-commits" "--model" "openrouter/deepseek/deepseek-coder"))
+(setq aider-args '("--no-auto-commits"
+                   "--model"
+                   "anthropic/claude-3.5-sonnet:beta"
+                   ;; "openrouter/deepseek/deepseek-coder"
+                   ))
 (setenv "OPENROUTER_API_KEY" (with-temp-buffer
                                (insert-file-contents "~/.config/openrouter/key.txt")
                                (string-trim (buffer-string))))
