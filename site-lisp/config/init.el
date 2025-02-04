d257410bd3538bfe6e17a5bcab7b3dd8582e1fb3
Andy Stewart <lazycat.manatee@gmail.com>
2025-02-04 12:43:17 +0800

    Load theme with sunrise.

--- site-lisp/config/init.el
+++ site-lisp/config/init.el
@@ -35,8 +35,8 @@
 
     (require 'init-generic)
     (require 'lazycat-theme)
-    ;; (lazycat-theme-load-with-sunrise)
-    (lazycat-theme-load-dark)
+    (lazycat-theme-load-with-sunrise)
+    ;; (lazycat-theme-load-dark)
     ;; (lazycat-theme-load-light)
     (when (featurep 'cocoa)
       (require 'cache-path-from-shell))
